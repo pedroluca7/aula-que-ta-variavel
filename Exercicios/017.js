bf458f2be5644f545326e9867aa635639233e5c6
Pedro luca <pedrinluca270@gmail.comgit config --listgit config --global user.email pedrinluca270@gmail.com>
2025-09-23 12:12:15 -0300

    clipa

---
+++ Exercicios/017.js
@@ -0,0 +1,20 @@
+const soma = (a, b) => {
+    return a + b;
+}
+console.log(soma(5, 3));
+const subtracao = (a, b) => {
+    return a - b;
+}
+console.log(subtracao(5, 3));
+const multiplicacao = (a, b) => {
+    return a * b;
+}
+console.log(multiplicacao(5, 3));
+const divisao = (a, b) => {
+    return a / b;
+}
+console.log(divisao(6, 3));
+const potencia = (a, b) => {
+    return a ** b;
+}
+
